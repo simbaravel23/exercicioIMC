156d60debce7e1802c3e61be86e4bd068f786638
RAVEL SIMBA <156199207+simbaravel23@users.noreply.github.com>
2024-01-25 18:28:38 -0300

    tentativa de realizar o codigo

--- imc exercicios/script.js
+++ imc exercicios/script.js
@@ -1,1 +1,10 @@
-alert('Ola')
+function calcular(){
+    const peso = document.getElementById('txtp').value
+    const altura = document.getElementById('txta').value
+    const res = document.getElementById('txtres').value
+
+    if ( altura !== '' &&  peso !== '') {
+
+    const valorIMC = (peso/(altura**2)).toFixed(2);
+    }
+}
