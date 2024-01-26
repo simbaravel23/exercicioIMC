a862cf0361f8f502faafa8339b3b02df7530c303
RAVEL SIMBA <156199207+simbaravel23@users.noreply.github.com>
2024-01-25 21:48:39 -0300

    teste de if

--- imc exercicios/script.js
+++ imc exercicios/script.js
@@ -6,6 +6,31 @@ function calcular() {
   var n1 = Number(tn1.value)
   var n2 = Number(tn2.value)
   var s = n1 / (n2**2)
-  res.innerHTML = s
-  
+
+  res.innerHTML = `O seu IMC é ${s}`
+
 }
+
+  
+   var classificacao = document.getElementById('classi')
+
+   
+
+   if (res <= 18.5){
+        classificacao.innerHTML = 'Está classificado como abaixo do peso.';
+    }else if (res <= 25) {
+        classificacao.innerHTML = 'Está classificadocom o peso ideal. Parabéns!';
+    }else if (res <= 30){
+        classificacao.innerHTML = 'Está classificadolevemente acima do peso.';
+    }else if (res <= 35){
+        classificacao.innerHTML = 'Está classificadocom obesidade grau I.';
+    }else if (res <= 40){
+        classificacao.innerHTML = 'Está classificado com obesidade grau II.';
+   }else  {
+        classificacao.innerHTML = 'Está classificado com obesidade grau III.';
+    } 
+        
+    
+
+   
+  
