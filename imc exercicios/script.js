af5d24e6be8525a8a716b9bb6135e53bcc4f7be5
RAVEL SIMBA <156199207+simbaravel23@users.noreply.github.com>
2024-01-26 22:54:06 -0300

    Atualizar o script.js

--- imc exercicios/script.js
+++ imc exercicios/script.js
@@ -17,17 +17,17 @@ function calcular() {
    
 
    if (res <= 18.5){
-        classificacao.innerHTML = 'Está classificado como abaixo do peso.';
-    }else if (res <= 25) {
-        classificacao.innerHTML = 'Está classificadocom o peso ideal. Parabéns!';
-    }else if (res <= 30){
-        classificacao.innerHTML = 'Está classificadolevemente acima do peso.';
-    }else if (res <= 35){
-        classificacao.innerHTML = 'Está classificadocom obesidade grau I.';
-    }else if (res <= 40){
-        classificacao.innerHTML = 'Está classificado com obesidade grau II.';
+        classificacao.innerText = 'Está classificado como abaixo do peso.';
+    }else if (res < 25) {
+        classificacao.innerText = 'Está classificadocom o peso ideal. Parabéns!';
+    }else if (res < 30){
+        classificacao.innerText = 'Está classificadolevemente acima do peso.';
+    }else if (res < 35){
+        classificacao.innerText = 'Está classificadocom obesidade grau I.';
+    }else if (res < 40){
+        classificacao.innerText = 'Está classificado com obesidade grau II.';
    }else  {
-        classificacao.innerHTML = 'Está classificado com obesidade grau III.';
+        classificacao.innerText = 'Está classificado com obesidade grau III.';
     } 
         
     
